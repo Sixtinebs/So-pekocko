eaa3f4cce14de65e1302d221f33fdee82a314004
sixtine <sixtine.bs11@gmail.com>
2021-03-12 12:29:39 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+const server = http.createServer()
+server.on('request', (req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+    res.end('Okay !')
+})
+server.listen(process.env.PORT || 3000);
+const express = require('express');
+
+const app = express();
+
+
+
+module.exports = app;
