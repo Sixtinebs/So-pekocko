7791efd6e185bfe8c59632ba6f8a787bc735d6b8
sixtine <sixtine.bs11@gmail.com>
2021-03-22 22:19:56 +0100

    get One sauce

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -7,24 +7,22 @@ exports.createSauce = (req, res, next) => {
         ...sauceObject,
         imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
     });
-    console.log(sauceObject.imageUrl);
     sauce.save()
         .then(() => res.status(201).json({ message: "La sauce a bien été créée ! :) " }))
         .catch(error => res.status(400).json({ error }));
 };
 
 exports.getAllSauces = (req, res, next) => {
-    console.log(Sauce.find());
     Sauce.find()
         .then(sauces => res.status(200).json( sauces ))
-        .then(sauces => console.log(sauces.imageUrl))
         .catch(error => res.satus(404).json({ error }));
 };
 
 exports.getOneSauce = (req, res, next) => {
+    console.log(req.params);
     Sauce.findOne({
         _id: req.params.id
     })
-        .then(sauce => res.status(200).json({ sauce: sauce._id }))
+        .then(sauce => res.status(200).json( sauce ))
         .catch(error => res.satus(404).json({ error }));
 };
